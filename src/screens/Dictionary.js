45fe10493a1b58c5eedf09853c9985a0630a79ba
Gorka <gorka@aquigorka.com>
2019-11-01 15:06:06 +0100

    Dictionary: add rows and row status updates

--- src/screens/Dictionary.js
+++ src/screens/Dictionary.js
@@ -1,27 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { useStore } from '../Store'
 
 function useDictionary() {
   const { id } = useParams()
   const {
-    state: { dictionaries },
-    dictionariesGet,
+    dictionaries,
+    dictionary,
+    dictionarySet,
+    dictionaryUnset,
+    dictionaryRowsNew,
+    dictionaryRowsDelete,
+    dictionaryRowsEdit,
   } = useStore()
-  const [dictionary, setDictionary] = useState(null)
+  const handleNewRow = useCallback(row => dictionaryRowsNew(id, row), [
+    dictionaryRowsNew,
+    id,
+  ])
+  const handleDeleteRow = useCallback(
+    rowId => dictionaryRowsDelete(id, rowId),
+    [dictionaryRowsDelete, id],
+  )
+  const handleEditRow = useCallback(
+    (rowId, row) => dictionaryRowsEdit(id, rowId, row),
+    [dictionaryRowsEdit, id],
+  )
 
   useEffect(
     () => {
-      setDictionary(dictionariesGet(id))
+      dictionarySet(id)
     },
-    [id, dictionariesGet, dictionaries],
+    [id, dictionarySet, dictionaries],
   )
 
-  return { dictionary }
+  // unmount
+  useEffect(() => () => dictionaryUnset(), [])
+
+  return {
+    dictionary,
+    onNewRow: handleNewRow,
+    onDeleteRow: handleDeleteRow,
+    onEditRow: handleEditRow,
+  }
 }
 
 function Dictionary() {
-  const { dictionary } = useDictionary()
+  const { dictionary, onNewRow, onDeleteRow, onEditRow } = useDictionary()
 
   if (!dictionary) {
     return (
@@ -31,7 +55,7 @@ function Dictionary() {
       </section>
     )
   }
-  const { id, name, status } = dictionary
+  const { id, name, status, rows } = dictionary
 
   return (
     <section>
@@ -41,8 +65,96 @@ function Dictionary() {
         <div>{name}</div>
         <div>{status}</div>
       </div>
+      <NewRow onNew={onNewRow} />
+      <ul>
+        {rows.map((item, index) => (
+          <Item
+            key={index}
+            {...item}
+            onEdit={onEditRow}
+            onDelete={onDeleteRow}
+          />
+        ))}
+      </ul>
     </section>
   )
 }
 
+function Item({ onEdit, onDelete, domain, range, rowId, rowStatus }) {
+  const handleDomainChange = useCallback(
+    ({ currentTarget: { value } }) => {
+      onEdit(rowId, { domain: value, range })
+    },
+    [range, onEdit, rowId],
+  )
+  const handleRangeChange = useCallback(
+    ({ currentTarget: { value } }) => {
+      onEdit(rowId, { domain, range: value })
+    },
+    [domain, onEdit, rowId],
+  )
+  const handleDelete = useCallback(
+    () => {
+      onDelete(rowId)
+    },
+    [onDelete, rowId],
+  )
+
+  return (
+    <li>
+      <div>{rowId}</div>
+      <div>{rowStatus}</div>
+      <div>
+        Domain: <input value={domain} onChange={handleDomainChange} />
+      </div>
+      <div>
+        Range: <input value={range} onChange={handleRangeChange} />
+      </div>
+      <div>
+        <button onClick={handleDelete}>delete</button>
+      </div>
+    </li>
+  )
+}
+
+function NewRow({ onNew }) {
+  const [domain, setDomain] = useState('')
+  const [range, setRange] = useState('')
+  const handleDomainChange = useCallback(({ currentTarget: { value } }) => {
+    setDomain(value)
+  }, [])
+  const handleRangeChange = useCallback(({ currentTarget: { value } }) => {
+    setRange(value)
+  }, [])
+  const handleSubmit = useCallback(
+    ev => {
+      ev.preventDefault()
+      if (!domain.trim() && !range.trim()) {
+        return
+      }
+      onNew({ domain, range })
+      setDomain('')
+      setRange('')
+    },
+    [domain, range, onNew],
+  )
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="New domain"
+        required
+        value={domain}
+        onChange={handleDomainChange}
+      />
+      <input
+        placeholder="New range"
+        required
+        value={range}
+        onChange={handleRangeChange}
+      />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
 export default Dictionary
