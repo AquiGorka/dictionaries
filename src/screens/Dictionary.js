4cca2200dd1ef3093b51b0f6b57a42c189b58b28
Gorka <gorka@aquigorka.com>
2019-11-01 10:46:06 +0100

    Dictionary: add boilerplate render for dictionary with eror check

--- src/screens/Dictionary.js
+++ src/screens/Dictionary.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import { useStore } from '../Store'
+
+function useDictionary() {
+  const { id } = useParams()
+  const { dictionariesGet } = useStore()
+  const [dictionary, setDictionary] = useState(null)
+  const [error, setError] = useState(false)
+
+  useEffect(
+    () => {
+      try {
+        const dictionary = dictionariesGet(id)
+        setDictionary(dictionary)
+      } catch (err) {
+        setError(true)
+      }
+    },
+    [id, dictionariesGet],
+  )
+
+  return { dictionary, error }
+}
 
 function Dictionary() {
-  return <div>Dictionary</div>
+  const { error, dictionary } = useDictionary()
+
+  if (error) {
+    return (
+      <section>
+        <p>Dictionary does not exist</p>
+        <Link to="/">Go back to dictionaries</Link>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <Link to="/">Dictionaries</Link>
+    </section>
+  )
 }
 
 export default Dictionary
