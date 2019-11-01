dfbc459ab4708e731df103478031ff073b5397d9
Gorka <gorka@aquigorka.com>
2019-11-01 16:47:50 +0100

    App: add layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Store } from './Store'
+import Layout from './components/Layout'
 import Dictionaries from './screens/Dictionaries'
 import Dictionary from './screens/Dictionary'
 
 function App() {
   return (
     <Store>
-      <Router>
-        <Switch>
-          <Route exact path={'/:id'} component={Dictionary} />
-          <Route path={'/'} component={Dictionaries} />
-        </Switch>
-      </Router>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path={'/:id'} component={Dictionary} />
+            <Route path={'/'} component={Dictionaries} />
+          </Switch>
+        </Router>
+      </Layout>
     </Store>
   )
 }
