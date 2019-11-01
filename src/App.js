97b2721e01776a39a459c6feaf78879b44945430
Gorka <gorka@aquigorka.com>
2019-11-01 09:55:16 +0100

    App: add store provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Store } from './Store'
 import Dictionaries from './screens/Dictionaries'
 import Dictionary from './screens/Dictionary'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={'/:id'} component={Dictionary} />
-        <Route path={'/'} component={Dictionaries} />
-      </Switch>
-    </Router>
+    <Store>
+      <Router>
+        <Switch>
+          <Route exact path={'/:id'} component={Dictionary} />
+          <Route path={'/'} component={Dictionaries} />
+        </Switch>
+      </Router>
+    </Store>
   )
 }
 
