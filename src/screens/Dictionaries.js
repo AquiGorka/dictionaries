fff92bfe68bf2e77c0c63b7c889b2cefa14cc744
Gorka <gorka@aquigorka.com>
2019-11-01 16:48:01 +0100

    Dictionaries: add some tyling

--- src/screens/Dictionaries.js
+++ src/screens/Dictionaries.js
@@ -1,6 +1,8 @@
 import React, { useState, useCallback } from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { useStore } from '../Store'
+import { Ul, Input, Button } from '../components/BaseElements'
 
 function useDictionaries() {
   const { dictionaries, dictionariesNew, dictionariesDelete } = useStore()
@@ -17,33 +19,105 @@ function Dictionaries() {
   const { dictionaries, onNew, onDelete, onEdit } = useDictionaries()
 
   return (
-    <section>
+    <Section>
       <NewDictionary onNew={onNew} />
-      <ul>
+      <List>
         {dictionaries.map(item => (
           <Item key={item.id} {...item} onDelete={onDelete} onEdit={onEdit} />
         ))}
-      </ul>
-    </section>
+      </List>
+    </Section>
   )
 }
+
+const Section = styled.section`
+  display: grid;
+  grid-gap: 16px;
+`
+
+const List = styled(Ul)`
+  & li:nth-child(2n + 1) {
+    background: #fafafa;
+  }
+
+  @media (min-width: 640px) {
+    border: 1px solid #eee;
+  }
+`
 
 function Item({ id, name, consistent, onDelete }) {
   const handleDelete = useCallback(() => onDelete(id), [id, onDelete])
 
   return (
-    <li>
-      <div>
-        <Link to={`/${id}`}>{id}</Link>
-      </div>
-      <div>{name}</div>
-      <div>consistent: {consistent ? 'yes' : 'no'}</div>
-      <div>
-        <button onClick={handleDelete}>X</button>
-      </div>
-    </li>
+    <Li consistent={consistent}>
+      <Name as={Link} to={`/${id}`}>
+        {name}
+      </Name>
+      {!consistent && <NotConsistent />}
+      <Remove onClick={handleDelete}>-</Remove>
+    </Li>
   )
 }
+
+const Name = styled.div`
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  padding: 24px 0;
+`
+
+const Li = styled.li`
+  display: grid;
+  grid-template-columns: ${({ consistent }) =>
+    consistent ? '1fr auto' : '1fr auto auto'};
+  grid-gap: 8px;
+  align-items: center;
+  padding: 0 8px;
+`
+
+const Remove = styled(Button)`
+  height: 40px;
+  width: 40px;
+  color: white;
+  border: 1px solid rgba(167, 40, 40, 0.4);
+  color: rgba(167, 40, 40, 0.4);
+  background: transparent;
+  user-select: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  line-height: 0px;
+`
+
+function NotConsistent() {
+  return (
+    <Error>
+      <Cross>x</Cross>
+      <Info>Errors</Info>
+    </Error>
+  )
+}
+
+const Error = styled.div`
+  user-select: none;
+  display: flex;
+  flex-direction: column;
+  height: 40px;
+  width: 40px;
+  border: 1px solid #eee;
+  align-items: center;
+  justify-content: center;
+  color: #AAA;
+`
+
+const Cross = styled.div`
+  font-size: 20px;
+`
+
+const Info = styled.div`
+  font-size: 10px;
+`
 
 function NewDictionary({ onNew }) {
   const [name, setName] = useState('')
@@ -66,16 +140,27 @@ function NewDictionary({ onNew }) {
   )
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="New name"
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="New dictionary name"
         required
         value={name}
         onChange={handleNameChange}
       />
-      <button type="submit">Add</button>
-    </form>
+      <Button>Add new dictionary</Button>
+    </Form>
   )
 }
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  grid-gap: 16px;
+  padding: 4px;
+
+  @media (min-width: 640px) {
+    padding: 0;
+  }
+`
 
 export default Dictionaries
