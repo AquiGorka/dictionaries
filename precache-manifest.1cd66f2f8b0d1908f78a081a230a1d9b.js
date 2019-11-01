83304894c5610c4edf7009219afe477b2c212f4d
AquiGorka <AquiGorka@users.noreply.github.com>
2019-11-01 18:58:27 +0000

    Automated deployment: Fri Nov  1 18:58:27 UTC 2019 43c1b265d195ee40fe32e3a3445bd2acf1104ce3

---
+++ precache-manifest.1cd66f2f8b0d1908f78a081a230a1d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e45cfe347fd878fdeee244dbc087009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f31e3cd5f24b3540b1bb",
+    "url": "/static/css/main.2d3c51e4.chunk.css"
+  },
+  {
+    "revision": "4827fef8ec0af950566e",
+    "url": "/static/js/2.cfb5d961.chunk.js"
+  },
+  {
+    "revision": "f31e3cd5f24b3540b1bb",
+    "url": "/static/js/main.fcd48daa.chunk.js"
+  },
+  {
+    "revision": "dbad8c744bf9a4bca6e6",
+    "url": "/static/js/runtime-main.1f40e637.js"
+  }
+]);
