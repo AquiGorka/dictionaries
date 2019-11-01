4f05b9ec8b94500e6ca3396fa3413e7c2d865b30
AquiGorka <AquiGorka@users.noreply.github.com>
2019-11-01 19:02:04 +0000

    Automated deployment: Fri Nov  1 19:02:04 UTC 2019 a49b58c47129ef4b4d8eca46fdeba29485392d0f

---
+++ precache-manifest.939387ce80af551df7818998fae1ea9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831ebc752488149c147ea5bb5cc2b273",
+    "url": "/dictionaries/index.html"
+  },
+  {
+    "revision": "a5f9c471858c1cdda00a",
+    "url": "/dictionaries/static/css/main.2d3c51e4.chunk.css"
+  },
+  {
+    "revision": "acdf11ffad3e1a8fc10f",
+    "url": "/dictionaries/static/js/2.b439bfa4.chunk.js"
+  },
+  {
+    "revision": "a5f9c471858c1cdda00a",
+    "url": "/dictionaries/static/js/main.2ff95fc0.chunk.js"
+  },
+  {
+    "revision": "76d13d798dd478d0971f",
+    "url": "/dictionaries/static/js/runtime-main.42fd44ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cd66f2f8b0d1908f78a081a230a1d9b.js"
+  "/dictionaries/precache-manifest.939387ce80af551df7818998fae1ea9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dictionaries/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
