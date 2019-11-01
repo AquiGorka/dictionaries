9820a961316cee95a399be8185c67d0760908598
Gorka <gorka@aquigorka.com>
2019-11-01 17:49:54 +0100

    Dictionary: add some styling

--- src/screens/Dictionary.js
+++ src/screens/Dictionary.js
@@ -1,6 +1,14 @@
 import React, { useCallback, useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { useParams, Link } from 'react-router-dom'
-import { useStore } from '../Store'
+import { useStore, STATUS } from '../Store'
+import {
+  Button,
+  DeleteButton,
+  Input,
+  Section,
+  Ul,
+} from '../components/BaseElements'
 
 function useDictionary() {
   const { id } = useParams()
@@ -55,30 +63,35 @@ function Dictionary() {
       </section>
     )
   }
-  const { id, name, consistent, rows } = dictionary
+  const { name, consistent, rows } = dictionary
 
   return (
-    <section>
-      <Link to="/">Dictionaries</Link>
+    <Section>
+      <Link to="/">/Dictionaries</Link>
       <div>
-        <div>{id}</div>
         <div>{name}</div>
-        <div>Consistent: {consistent ? 'yes' : 'no'}</div>
       </div>
       <NewRow onNew={onNewRow} />
-      <ul>
-        {rows.map((item, index) => (
-          <Item
-            key={index}
-            {...item}
-            onEdit={onEditRow}
-            onDelete={onDeleteRow}
-          />
-        ))}
-      </ul>
-    </section>
+      {!!rows.length && (
+        <Ul>
+          {rows.map((item, index) => (
+            <Item
+              key={index}
+              {...item}
+              onEdit={onEditRow}
+              onDelete={onDeleteRow}
+            />
+          ))}
+        </Ul>
+      )}
+    </Section>
   )
 }
+
+const Info = styled.div`
+  color: rgb(167, 40, 40);
+  font-size: 12px;
+`
 
 function Item({ onEdit, onDelete, domain, range, rowId, rowStatus }) {
   const handleDomainChange = useCallback(
@@ -101,21 +114,64 @@ function Item({ onEdit, onDelete, domain, range, rowId, rowStatus }) {
   )
 
   return (
-    <li>
-      <div>{rowId}</div>
-      <div>{rowStatus}</div>
+    <Li error={rowStatus !== STATUS.OK}>
+      <Label>
+        <Type>Domain</Type>
+        <Field value={domain} onChange={handleDomainChange} />
+      </Label>
+      <Label>
+        <Type>Range</Type>
+        <Field value={range} onChange={handleRangeChange} />
+      </Label>
       <div>
-        Domain: <input value={domain} onChange={handleDomainChange} />
+        <Error>{rowStatus}</Error>
       </div>
       <div>
-        Range: <input value={range} onChange={handleRangeChange} />
+        <DeleteButton onClick={handleDelete}>-</DeleteButton>
       </div>
-      <div>
-        <button onClick={handleDelete}>delete</button>
-      </div>
-    </li>
+    </Li>
   )
 }
+
+const Li = styled.li`
+  display: grid;
+  grid-template-columns: ${({ error }) =>
+    error ? '1fr 1fr auto auto' : '1fr 1fr auto auto'};
+  grid-gap: 8px;
+  align-items: center;
+  padding: 0 8px;
+`
+
+const Label = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr;
+`
+
+const Field = styled.input`
+  border: none;
+  background: none;
+  border-bottom: 1px solid #ccc;
+  padding: 4px;
+`
+
+const Type = styled.div`
+  font-size: 12px;
+  color: #ccc;
+`
+
+const Error = styled.div`
+  border-radius: 4px;
+  user-select: none;
+  display: flex;
+  flex-direction: column;
+  height: 40px;
+  width: 40px;
+  border: 1px solid #eee;
+  align-items: center;
+  justify-content: center;
+  border: #aaa;
+  color: #bbb;
+`
 
 function NewRow({ onNew }) {
   const [domain, setDomain] = useState('')
@@ -140,21 +196,33 @@ function NewRow({ onNew }) {
   )
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        placeholder="New domain"
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="Domain"
         required
         value={domain}
         onChange={handleDomainChange}
       />
-      <input
-        placeholder="New range"
+      <Input
+        placeholder="Range"
         required
         value={range}
         onChange={handleRangeChange}
       />
-      <button type="submit">Add</button>
-    </form>
+      <Button type="submit">Add new row</Button>
+    </Form>
   )
 }
+
+const Form = styled.form`
+  display: grid;
+  grid-template-columns: 1fr 1fr max-content;
+  grid-gap: 16px;
+  padding: 4px;
+
+  @media (min-width: 640px) {
+    padding: 0;
+  }
+`
+
 export default Dictionary
