fc1f9fb3d801bae1d12e655a31345fae82252fb3
Gorka <gorka@aquigorka.com>
2019-11-01 20:11:37 +0100

    App: add basename and public url for deployment

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Layout from './components/Layout'
 import Dictionaries from './screens/Dictionaries'
 import Dictionary from './screens/Dictionary'
 
+const { PUBLIC_URL = '' } = process.env
+
 function App() {
   return (
     <Store>
       <Layout>
-        <Router>
+        <Router basename={'/dictionaries'}>
           <Switch>
-            <Route exact path={'/:id'} component={Dictionary} />
-            <Route path={'/'} component={Dictionaries} />
+            <Route exact path={`${PUBLIC_URL}/:id`} component={Dictionary} />
+            <Route path={`${PUBLIC_URL}/`} component={Dictionaries} />
           </Switch>
         </Router>
       </Layout>
