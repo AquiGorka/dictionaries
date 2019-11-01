ffd3cae54a63555d7cc2f30aeed20de35858a1ad
Gorka <gorka@aquigorka.com>
2019-11-01 15:04:41 +0100

    Store: refactor state to independent dictionaries and dictionary
    Adds row status updates

--- src/Store.js
+++ src/Store.js
@@ -4,15 +4,92 @@ import uuid from 'uuid/v4'
 const DICTIONARIES = 'dictionaries'
 const STATUS = {
   EMPTY: 'empty',
+  OK: 'ok',
+  DUPLICATE: 'duplicate',
+  FORK: 'fork',
+  CYCLE: 'cycle',
+  CHAIN: 'chain',
+  NEW: 'new',
+}
+
+function compareLowerCase(str1, str2) {
+  return str1.toLowerCase() === str2.toLowerCase()
+}
+
+function isThisACycle(item, arr) {
+  const loop = arr.find(i => compareLowerCase(item.range, i.domain))
+  if (!loop) {
+    return false
+  }
+  if (compareLowerCase(loop.range, item.domain)) {
+    return true
+  }
+  return isThisACycle(loop, arr)
+}
+
+function computeDictionaryStatus(rows) {
+  // duplicates
+
+  // forks
+
+  // cycles
+
+  // chains
+
+  return STATUS.OK
+}
+
+function updateRowsStatus(arr) {
+  return arr.map(item => ({
+    ...item,
+    rows: item.rows.map((row, _, rows) => ({
+      ...row,
+      rowStatus: computeRowStatus(row, rows),
+    })),
+  }))
+}
+
+function computeRowStatus(row, rows) {
+  // this is the only item
+  if (rows.length === 1) {
+    return STATUS.OK
+  }
+  // duplicate
+  if (
+    rows.find(
+      r =>
+        r.rowId !== row.rowId &&
+        compareLowerCase(r.domain, row.domain) &&
+        compareLowerCase(r.range, row.range),
+    )
+  ) {
+    return STATUS.DUPLICATE
+  }
+  // fork
+  if (
+    rows.find(
+      r => r.rowId !== row.rowId && compareLowerCase(r.domain, row.domain),
+    )
+  ) {
+    return STATUS.FORK
+  }
+  // cycle
+  if (isThisACycle(row, rows)) {
+    return STATUS.CYCLE
+  }
+  // chain
+  if (rows.find(r => compareLowerCase(row.range, r.domain))) {
+    return STATUS.CHAIN
+  }
+  // ok
+  return STATUS.OK
 }
 
 const Context = React.createContext()
 
 function Store({ children }) {
-  const [state, setState] = useState({
-    dictionaries: [],
-    dictionary: null,
-  })
+  const [dictionaries, setDictionaries] = useState([])
+  const [dictionary, setDictionary] = useState(null)
 
   const dictionariesNew = name => {
     const newDictionary = {
@@ -21,23 +98,120 @@ function Store({ children }) {
       status: STATUS.EMPTY,
       rows: [],
     }
-    setState(state => ({
-      ...state,
-      dictionaries: [...state.dictionaries, newDictionary],
-    }))
+    setDictionaries([...dictionaries, newDictionary])
   }
 
   const dictionariesDelete = id => {
-    setState(state => ({
-      ...state,
-      dictionaries: state.dictionaries.filter(d => d.id !== id),
-    }))
+    setDictionaries(dictionaries.filter(d => d.id !== id))
   }
 
-  const dictionariesGet = id => {
-    return state.dictionaries.find(d => d.id === id)
+  const dictionarySet = id => {
+    const item = dictionaries.find(d => d.id === id)
+    if (!item) {
+      return
+    }
+    setDictionary(item)
   }
 
+  const dictionaryUnset = () => {
+    setDictionary(null)
+  }
+
+  const dictionaryRowsNew = (id, row) => {
+    if (!dictionary) {
+      throw new Error('A dictionary has not been selected')
+    }
+
+    const rowId = uuid()
+    const rowStatus = STATUS.NEW
+    const newRow = { ...row, rowId, rowStatus }
+    const updatedDictionary = {
+      ...dictionary,
+      rows: [...dictionary.rows, newRow],
+    }
+    const updatedDictionaries = dictionaries.map(
+      d => (d.id === id ? updatedDictionary : d),
+    )
+    const updatedDictionariesWithUpdatedRows = updateRowsStatus(
+      updatedDictionaries,
+    )
+    setDictionaries(updatedDictionariesWithUpdatedRows)
+
+    // side effect: update current
+    setDictionary(updatedDictionary)
+  }
+
+  const dictionaryRowsDelete = (id, rowId) => {
+    if (!dictionary) {
+      throw new Error('A dictionary has not been selected')
+    }
+
+    const updatedDictionary = {
+      ...dictionary,
+      rows: [...dictionary.rows.filter(r => r.rowId !== rowId)],
+    }
+    const updatedDictionaries = dictionaries.map(
+      d => (d.id === id ? updatedDictionary : d),
+    )
+    const updatedDictionariesWithUpdatedRows = updateRowsStatus(
+      updatedDictionaries,
+    )
+    setDictionaries(updatedDictionariesWithUpdatedRows)
+
+    // side effect: update current
+    setDictionary(updatedDictionary)
+  }
+
+  const dictionaryRowsEdit = (id, rowId, row) => {
+    if (!dictionary) {
+      throw new Error('A dictionary has not been selected')
+    }
+
+    const updatedDictionary = {
+      ...dictionary,
+      rows: [
+        ...dictionary.rows.map(r => (r.rowId !== rowId ? r : { ...r, ...row })),
+      ],
+    }
+    const updatedDictionaries = dictionaries.map(
+      d => (d.id === id ? updatedDictionary : d),
+    )
+    const updatedDictionariesWithUpdatedRows = updateRowsStatus(
+      updatedDictionaries,
+    )
+    setDictionaries(updatedDictionariesWithUpdatedRows)
+
+    // side effect: update current
+    setDictionary(updatedDictionary)
+  }
+
+  const dictionaryUpdateStatus = () => {
+    let newStatus = STATUS.EMPTY
+    const { rows } = dictionary
+
+    if (rows.length) {
+      newStatus = computeDictionaryStatus()
+    }
+
+    if (dictionary.status === newStatus) {
+      return
+    }
+
+    //setDictionary(updatedDictionary)
+    //setDictionaries(
+    //dictionaries.map(d => (d.id === id ? updatedDictionary : d)),
+    //)
+  }
+
+  // when rows change update status
+  useEffect(
+    () => {
+      //dictionaryUpdateStatus()
+    },
+    [dictionary],
+  )
+
+  // one time: load local storage data
   useEffect(() => {
     const data = localStorage.getItem(DICTIONARIES)
     try {
@@ -46,7 +220,7 @@ function Store({ children }) {
         if (!parsedData.length) {
           return
         }
-        setState(state => ({ ...state, dictionaries: parsedData }))
+        setDictionaries(parsedData)
       }
     } catch (e) {
       console.warn(
@@ -55,16 +229,27 @@ function Store({ children }) {
     }
   }, [])
 
+  // when dictionaries changes persist data to local storage
   useEffect(
     () => {
-      localStorage.setItem(DICTIONARIES, JSON.stringify(state.dictionaries))
+      localStorage.setItem(DICTIONARIES, JSON.stringify(dictionaries))
     },
-    [state.dictionaries],
+    [dictionaries],
   )
 
   return (
     <Context.Provider
-      value={{ state, dictionariesNew, dictionariesDelete, dictionariesGet }}
+      value={{
+        dictionary,
+        dictionaries,
+        dictionariesNew,
+        dictionariesDelete,
+        dictionarySet,
+        dictionaryUnset,
+        dictionaryRowsNew,
+        dictionaryRowsDelete,
+        dictionaryRowsEdit,
+      }}
     >
       {children}
     </Context.Provider>
