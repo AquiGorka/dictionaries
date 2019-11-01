cbc79f0db06fa30112362d1d73950582808f2499
Gorka <gorka@aquigorka.com>
2019-11-01 16:47:04 +0100

    BaseComponents: add base styled elements

---
+++ src/components/BaseElements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Input = styled.input`
+  padding: 8px;
+`
+
+const Ul = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+`
+
+const Button = styled.button`
+  border-radius: 4px;
+  padding: 8px
+  border: 1px solid #719fce;
+  background: rgba(113, 159, 206, 0.3);
+  cursor: pointer;
+`
+
+export { Ul, Input, Button }
